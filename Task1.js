2bfd9905c960a2b6afa3d563e0b3e25dd485dd79
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-09-13 20:14:46 +0300

    done

---
+++ Task1.js
@@ -0,0 +1,23 @@
+// Q 1
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+// Q 2
+for (let i = 2; i <= 20; i = i + 2) {
+  console.log(i);
+}
+// Q 3
+let x = 1;
+let sum = 0;
+while (x <= 100) {
+  sum = sum + x;
+  x++;
+}
+console.log(sum);
+// Q 4
+let myArray = [5, 5, 7, 5];
+let sum2 = 0;
+for (let i = 0; i < myArray.length; i++) {
+  sum2 = sum2 + myArray[i];
+}
+console.log(sum2);
